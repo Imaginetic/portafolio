27dcd4b222d3b757d658d74fb2534af1f5aa1a60
Imaginetic <imaginetic.studios@gmail.com>
2019-09-04 18:01:02 -0400

    Primer version PORTAFOLIO

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+	$(window).scroll(function(){
+		var barra = $(window).scrollTop();
+		var posicion =  (barra * 4);
+
+		$('body').css({
+			 background-position: 40px;
+		});
+
+	});
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  });
+	$(function(){
+	  $("a").click(function(event){
+	    if (this.hash !== "") {
+	      event.preventDefault();
+
+	      var gato = this.hash;
+
+	      $("html, body").animate({
+	        scrollTop: $(gato).offset().top
+	      }, 800, function(){
+	        window.location.hash= gato;
+	      });
+	    }
+	  });
+});
